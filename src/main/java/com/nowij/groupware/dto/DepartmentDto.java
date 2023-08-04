f19ca190a3f44de1ba8f0297d404d3d67274c5f4
nowij <nowijdl@gmail.com>
2023-08-04 14:56:14 +0900

    부서, 직위 엔티티 추가

---
+++ src/main/java/com/nowij/groupware/dto/DepartmentDto.java
@@ -0,0 +1,9 @@
+package com.nowij.groupware.dto;
+
+import lombok.Data;
+
+@Data
+public class DepartmentDto {
+    private String deptCode;
+    private String deptName;
+}
---
+++ src/main/java/com/nowij/groupware/dto/PositionDto.java
@@ -0,0 +1,9 @@
+package com.nowij.groupware.dto;
+
+import lombok.Data;
+
+@Data
+public class PositionDto {
+    private String positionCode;
+    private String positionName;
+}
