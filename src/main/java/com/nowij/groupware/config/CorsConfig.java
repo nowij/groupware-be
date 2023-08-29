aace56a568200fa66cf30f149984a2906f1b115e
nowij <nowijdl@gmail.com>
2023-08-29 15:30:21 +0900

    cors 설정 중복 - 주석처리

--- src/main/java/com/nowij/groupware/config/CorsConfig.java
+++ src/main/java/com/nowij/groupware/config/CorsConfig.java
@@ -1,30 +1,30 @@
-package com.nowij.groupware.config;
-
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Collections;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
-        //config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
-
-}
+//package com.nowij.groupware.config;
+//
+//import org.apache.catalina.filters.CorsFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//
+//        config.setAllowCredentials(true);
+//        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+//        //config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
+//
+//}
